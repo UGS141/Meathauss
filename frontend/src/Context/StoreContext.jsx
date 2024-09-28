682ec3276aff5b6c7d1c8a66e7019a7ba9e97548
UGS141 <ugssolutions1@gmail.com>
2024-09-28 18:56:55 +0530

    first commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "https://meathaussstore.onrender.com";
+  const url = "http://localhost:4000";
   const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
